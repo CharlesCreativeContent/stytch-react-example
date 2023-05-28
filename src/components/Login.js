ea8d71d924bb32848138807adcb3b8b3645bf2d9
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2023-05-27 20:44:37 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,16 +23,16 @@ const Login = () => {
   const config = {
     products: [Products.emailMagicLinks, Products.oauth],
     emailMagicLinksOptions: {
-      loginRedirectURL: "http://localhost:3000",
+      loginRedirectURL: "https://stytch-react-template.vercel.app/",
       loginExpirationMinutes: 60,
-      signupRedirectURL: "http://localhost:3000",
+      signupRedirectURL: "https://stytch-react-template.vercel.app/",
       signupExpirationMinutes: 60,
     },
     oauthOptions: {
       providers: [{ type: "google" }],
-      loginRedirectURL: "http://localhost:3000",
+      loginRedirectURL: "https://stytch-react-template.vercel.app/",
       loginExpirationMinutes: 60,
-      signupRedirectURL: "http://localhost:3000",
+      signupRedirectURL: "https://stytch-react-template.vercel.app/",
       signupExpirationMinutes: 60,
     },
   };
